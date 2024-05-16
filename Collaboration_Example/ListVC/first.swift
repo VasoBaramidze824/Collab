612c427cc2c448c9896f49b838ae32c29bcf0a12
Davit Natenadze <davit.natenadze@gmail.com>
2024-05-16 19:48:07 +0200

    123123123123123

---
+++ Collaboration_Example/ListVC/first.swift
@@ -0,0 +1,20 @@
+//
+//  first.swift
+//  Collaboration_Example
+//
+//  Created by Davit Natenadze on 16.05.24.
+//
+
+import Foundation
+
+
+///
+//
+
+
+
+
+
+
+
+
---
+++ Collaboration_Example/ListVC/second.swift
@@ -0,0 +1,19 @@
+//
+//  second.swift
+//  Collaboration_Example
+//
+//  Created by Davit Natenadze on 16.05.24.
+//
+
+import Foundation
+
+
+
+
+
+
+
+
+
+
+
